fa2300d2977d504f7ce7f7720afe1f4a28cb8e5c
Hunyue Yau <hy-git@hy-research.com>
2012-02-27 01:04:31 -0800

    Add support for the mux control in front of the mcp3001.
    This configuration should probally come in through the platform data.
    But it is functional enough for the time being.
    
    Signed-off-by: Hunyue Yau <hy-git@hy-research.com>

--- drivers/hwmon/mcp3001.c
+++ drivers/hwmon/mcp3001.c
@@ -23,10 +23,18 @@
 #include <linux/device.h>
 #include <linux/err.h>
 #include <linux/hwmon.h>
+#include <linux/gpio.h>
 #include <linux/mutex.h>
 #include <linux/spi/spi.h>
 
 #define DRVNAME  "mcp3k1"
+
+#define ADC_MUX_ENABLE_GPIO	101
+#define ADC_MUX_A0_GPIO		102
+#define ADC_MUX_A1_GPIO		103
+#define ADC_MUX_A2_GPIO		104
+#define ADC_MUX_A3_GPIO		105
+#define ADC_MUX_A4_GPIO		72
 
 struct mcp3k1 {
 	struct device *hwmon_dev;
@@ -43,17 +51,35 @@ static ssize_t mcp3k1_show(struct device *dev, struct device_attribute *attr,
 	struct mcp3k1 *p_mcp3k1 = dev_get_drvdata(dev);
 	u8 rxbuf[3]; 
 	int val = 0;
+	int millivolt;
 
 	spi_write_then_read(p_mcp3k1->spi, NULL, 0, rxbuf, 2);
 
 	val = ((rxbuf[0] & 0x1f) << 5) | ((rxbuf[1] & 0xf8) >> 3);
+#if 0
+	/* Return raw value */
 	return sprintf(buf, "0x%0x", val);  // hack for now
+#else
+	millivolt = (val * 2500)/1024;
+	return sprintf(buf, "%i", millivolt);
+#endif
 }
 
 static ssize_t mcp3k1_store(struct device *dev, struct device_attribute *attr,
 			    const char *buf, size_t count)
 {
-	return sscanf(buf, "%du", &index);
+	int c;
+	c = sscanf(buf, "%du", &index);
+	if (c > 0) {
+		if (index >= 0 && index < (1 << 5)) {
+			gpio_set_value(ADC_MUX_A0_GPIO, ((index & (1 << 0))?1:0));
+			gpio_set_value(ADC_MUX_A1_GPIO, ((index & (1 << 1))?1:0));
+			gpio_set_value(ADC_MUX_A2_GPIO, ((index & (1 << 2))?1:0));
+			gpio_set_value(ADC_MUX_A3_GPIO, ((index & (1 << 3))?1:0));
+			gpio_set_value(ADC_MUX_A4_GPIO, ((index & (1 << 4))?1:0));
+		}
+	}
+	return count;
 }
 
 
@@ -68,8 +94,7 @@ static int __devinit mcp3k1_probe(struct spi_device *spi)
 	// CS == AL and MSB first are defaults, and correct for mcp3k1
 	err = spi_setup(spi);
 	if (err < 0)
-
-	return err;
+		return err;
 
 	p_mcp3k1 = kzalloc(sizeof *p_mcp3k1, GFP_KERNEL);
 	if (!p_mcp3k1)
@@ -78,6 +103,58 @@ static int __devinit mcp3k1_probe(struct spi_device *spi)
 	mutex_init(&p_mcp3k1->lock);
   
 	p_mcp3k1->spi = spi;
+
+	err = gpio_request(ADC_MUX_ENABLE_GPIO, "ADC Enable");
+	if (err < 0) {
+		return err;
+	}
+	err = gpio_request(ADC_MUX_A0_GPIO, "ADC A0");
+	if (err < 0) {
+		gpio_free(ADC_MUX_ENABLE_GPIO);
+		return err;
+	}
+	err = gpio_request(ADC_MUX_A1_GPIO, "ADC A1");
+	if (err < 0) {
+		gpio_free(ADC_MUX_ENABLE_GPIO);
+		gpio_free(ADC_MUX_A0_GPIO);
+		return err;
+	}
+	err = gpio_request(ADC_MUX_A2_GPIO, "ADC A2");
+	if (err < 0) {
+		gpio_free(ADC_MUX_ENABLE_GPIO);
+		gpio_free(ADC_MUX_A0_GPIO);
+		gpio_free(ADC_MUX_A1_GPIO);
+		return err;
+	}
+	err = gpio_request(ADC_MUX_A3_GPIO, "ADC A3");
+	if (err < 0) {
+		gpio_free(ADC_MUX_ENABLE_GPIO);
+		gpio_free(ADC_MUX_A0_GPIO);
+		gpio_free(ADC_MUX_A1_GPIO);
+		gpio_free(ADC_MUX_A2_GPIO);
+		return err;
+	}
+	err = gpio_request(ADC_MUX_A4_GPIO, "ADC A4");
+	if (err < 0) {
+		gpio_free(ADC_MUX_ENABLE_GPIO);
+		gpio_free(ADC_MUX_A0_GPIO);
+		gpio_free(ADC_MUX_A1_GPIO);
+		gpio_free(ADC_MUX_A2_GPIO);
+		gpio_free(ADC_MUX_A3_GPIO);
+		return err;
+	}
+	gpio_export(ADC_MUX_ENABLE_GPIO, 0);
+	gpio_direction_output(ADC_MUX_ENABLE_GPIO, 0);	/* Enable it */
+	gpio_export(ADC_MUX_A0_GPIO, 0);
+	gpio_direction_output(ADC_MUX_A0_GPIO, 0);
+	gpio_export(ADC_MUX_A1_GPIO, 0);
+	gpio_direction_output(ADC_MUX_A1_GPIO, 0);
+	gpio_export(ADC_MUX_A2_GPIO, 0);
+	gpio_direction_output(ADC_MUX_A2_GPIO, 0);
+	gpio_export(ADC_MUX_A3_GPIO, 0);
+	gpio_direction_output(ADC_MUX_A3_GPIO, 0);
+	gpio_export(ADC_MUX_A4_GPIO, 0);
+	gpio_direction_output(ADC_MUX_A4_GPIO, 0);
 
 	p_mcp3k1->hwmon_dev = hwmon_device_register(&spi->dev);
 	if (IS_ERR(p_mcp3k1->hwmon_dev)) {

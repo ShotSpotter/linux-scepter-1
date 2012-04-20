0dc9c4c053007b52ae7388d7d5a24246ddf3f0d1
Matthew R. Laue <matt@akirisolutions.com>
2012-04-20 14:41:06 -0700

    Checkin the sysfs module to control the scepter board heaters.

---
+++ drivers/misc/heater.c
@@ -0,0 +1,158 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/device.h>
+#include <linux/sysfs.h>
+#include <linux/stat.h>
+#include <linux/kdev_t.h>
+#include <linux/err.h>
+#include <linux/gpio.h>
+
+MODULE_AUTHOR("Matthew R. Laue");
+MODULE_LICENSE("GPL");
+
+struct heater {
+	struct device *dev;
+	int gpio;
+	const char *name;
+};
+
+#define CPU_ADC_HEAT_LFT 70
+#define CPU_ADC_HEAT_RGT 71
+
+static struct heater heaters[] = {
+	{ .dev = NULL, .gpio = CPU_ADC_HEAT_LFT, .name = "CPU_ADC_HEAT_LFT" },
+	{ .dev = NULL, .gpio = CPU_ADC_HEAT_RGT, .name ="CPU_ADC_HEAT_RGT" },
+};
+
+static ssize_t
+heater_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct heater *h = dev_get_drvdata(dev);
+	int value = gpio_get_value(h->gpio) ? 1 : 0;
+	return snprintf(buf, PAGE_SIZE, "%d\n", value);
+}
+
+static ssize_t
+heater_store(struct device *dev, struct device_attribute *attr,
+             const char *buf, size_t count)
+{
+	struct heater *h = dev_get_drvdata(dev);
+	if (count > 2) {
+		return -EINVAL;
+	} else if (count == 2 && buf[1] != '\n') {
+		return -EINVAL;
+	} else if (count == 0) {
+		return 0;
+	} else if (buf[0] == '0') {
+		gpio_set_value(h->gpio, 0);
+		return count;
+	} else if (buf[0] == '1') {
+		gpio_set_value(h->gpio, 1);
+		return count;
+	}
+	return -EINVAL;
+}
+
+static DEVICE_ATTR(value, S_IWUSR | S_IRUGO, heater_show, heater_store);
+
+static const struct attribute *heater_attrs[] = {
+	&dev_attr_value.attr,
+	NULL,
+};
+
+static const struct attribute_group heater_attr_group = {
+	.attrs = (struct attribute **) heater_attrs,
+};
+
+static struct class_attribute heater_class_attrs[] = {
+	__ATTR_NULL,
+};
+
+static struct class heater_class = {
+	.name = "heater",
+	.owner = THIS_MODULE,
+	.class_attrs = heater_class_attrs,
+};
+
+static int __init
+heater_dev_init(struct heater *h)
+{
+	int retval;
+	struct device *dev;
+
+	dev = device_create(&heater_class, NULL, MKDEV(0, 0),
+			    h, h->name);
+	if (IS_ERR(dev)) {
+		retval = PTR_ERR(dev);
+		printk(KERN_ERR "device_create failed for %s: %d\n",
+		       h->name, retval);
+		return retval;
+	}
+
+	retval = device_create_file(dev, &dev_attr_value);
+	if (retval != 0) {
+		printk(KERN_ERR "device_create_file failed for %s: %d\n",
+		       h->name, retval);
+		device_unregister(dev);
+		return retval;
+	}
+
+	retval = gpio_request(h->gpio, h->name);
+	if (retval != 0) {
+		printk(KERN_ERR "gpio_request for gpio%d failed: %d\n",
+		       h->gpio, retval);
+		device_unregister(dev);
+		return retval;
+	}
+
+	gpio_direction_output(h->gpio, 0);
+	h->dev = dev;
+
+	return 0;
+}
+
+static int __init
+heater_init(void)
+{
+	int retval, i;
+
+	retval = class_register(&heater_class);
+	if (retval < 0) {
+		printk(KERN_ERR "heater class register failed: %d\n", retval);
+		return retval;
+	}
+
+	for (i = 0; i < 2; i++) {
+		retval = heater_dev_init(&heaters[i]);
+		if (retval != 0) {
+			goto error;
+		}
+	}
+
+	printk(KERN_INFO "heater: initialized\n");
+	return 0;
+
+error:
+	for (i = 0; i < 2; i++) {
+		if (heaters[i].dev != NULL) {
+			device_unregister(heaters[i].dev);
+			gpio_free(heaters[i].gpio);
+		}
+	}
+	return retval;
+}
+
+static void __exit
+heater_exit(void)
+{
+	int i;
+	for (i = 0; i < 2; i++) {
+		device_unregister(heaters[i].dev);
+	}
+	class_unregister(&heater_class);
+	printk(KERN_INFO "heater: exit\n");
+}
+
+module_init(heater_init);
+module_exit(heater_exit);

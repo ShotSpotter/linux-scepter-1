394e98a658d54a583e519508af34737e22f21c0a
Sarah Newman <snewman@shotspotter.com>
2012-04-19 18:05:18 -0700

    Warn if omap_wdt is opened with no way out
    Signed-off-by: Sarah Newman <snewman@shotspotter.com>
    (cherry picked from commit 895eb5f1933149d24b27725535047c7f7a38986b)

--- drivers/watchdog/omap_wdt.c
+++ drivers/watchdog/omap_wdt.c
@@ -146,6 +146,10 @@ static int omap_wdt_open(struct inode *inode, struct file *file)
 	if (test_and_set_bit(1, (unsigned long *)&(wdev->omap_wdt_users)))
 		return -EBUSY;
 
+#ifdef CONFIG_WATCHDOG_NOWAYOUT
+	printk(KERN_CRIT "omap_wdt: opened with no way out!\n");
+#endif
+
 	clk_enable(wdev->ick);    /* Enable the interface clock */
 	clk_enable(wdev->fck);    /* Enable the functional clock */
 

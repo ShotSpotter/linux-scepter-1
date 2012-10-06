5b36e5550487ccf7d87ad667e5a0faf74528967b
Sarah Newman <snewman@shotspotter.com>
2012-10-05 20:53:50 -0700

    Add GPL header to heater.c
    Signed-off-by: Sarah Newman <snewman@shotspotter.com>

--- drivers/misc/heater.c
+++ drivers/misc/heater.c
@@ -1,3 +1,19 @@
+/*
+ * linux/drivers/misc/heater.c
+ *
+ * Copyright (C) 2012 HY Research LLC
+ * Author: Matthew R. Laue
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as  published by the
+ * Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any kind,
+ * whether express or implied; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
